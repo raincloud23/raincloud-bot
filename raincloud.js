7352c7a794c97d559034bc5770515edb6da06438
raincloud23 <39390708+raincloud23@users.noreply.github.com>
2019-02-13 21:05:16 -0800

    this is what im doing

--- raincloud.js
+++ raincloud.js
@@ -3,21 +3,31 @@ const client = new Discord.Client();
 const config= require('./config.json');
 const botToken = config.token;
 const prefix = '!';
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('message', msg => {
-  if(!message.guild || message.content.indexOf(prefix) > 0) return;
-  let msgArray = message.content.split(" ");
-  let cmd = msgArray[0].slice(prefix.length);
-  let args = msgArray.slice(1);
-  if (cmd === 'ping'){
-    message.channel.send('pong!');
-  } else if(cmd == 'foo'){
-    message.channel.send('bar!');
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+ 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
   }
+  if(command === "kick") {
+    let member = message.mentions.members.first();
+    let reason = args.slice(1).join(" ");
+    member.kick(reason);
+  };
+  if(command === "ban") {
+    let member = message.mentions.members.first();
+    let reason = args.slice(1).join(" ");
+    member.ban(reason);
+  };
 });
 client.login(botToken);
- 
